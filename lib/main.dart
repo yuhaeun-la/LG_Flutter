9281f2a4da193bd127eccfb4256d13cfe54fbdaa
yuhaeun-la <exok1and1m@gmail.com>
2024-08-30 15:52:52 +0900

    bugFIx:: 잘못된 import제거

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:little_guardian/ui/views/child/progress2_view.dart';
 import 'package:little_guardian/ui/views/parent/home_view.dart';
 import 'package:little_guardian/ui/views/landing_view.dart';
 import 'package:little_guardian/ui/views/child/progress1_view.dart';
-import 'package:little_guardian/ui/views/components/setting_progress.dart';
 
 void main() {
   runApp(const MyApp());
