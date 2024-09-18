810917b8d442a5c3bbbeaf580189e1cbd6dba510
yuhaeun-la <exok1and1m@gmail.com>
2024-09-18 15:50:50 +0900

    [Feat] 스플래쉬 뷰 화면설정

--- lib/ui/views/splash_view.dart
+++ lib/ui/views/splash_view.dart
@@ -1,38 +1,35 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:little_guardian/ui/style/colors_util.dart';
 
-class Splash_view extends StatelessWidget {
-  const Splash_view({super.key});
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: pointColor,
-      body: Container(
-        child: GestureDetector(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Image.asset(
-                //   'assets/mainIcon.png',
-                //   width: 100,
-                //   height: 100,
-                // ),
-                const SizedBox(height: 5),
-                const Text(
-                  'Littles',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 55,
-                    color: gray2,
-                    fontWeight: FontWeight.w900,
-                    fontFamily: 'jalnan'
-                  ),
+      body: GestureDetector(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Image.asset(
+              //   'assets/mainIcon.png',
+              //   width: 100,
+              //   height: 100,
+              // ),
+              const SizedBox(height: 5),
+              const Text(
+                'Littles',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 55,
+                  color: gray2,
+                  fontWeight: FontWeight.w900,
+                  fontFamily: 'jalnan'
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
