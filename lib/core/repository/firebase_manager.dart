58152029fa74d64861951c105d129db868b32c28
yuhaeun-la <exok1and1m@gmail.com>
2024-09-18 17:05:52 +0900

    [Feat/#06] 프로젝트 파이어베이스 연동

--- lib/core/repository/firebase_manager.dart
+++ lib/core/repository/firebase_manager.dart
@@ -1,50 +1,60 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-//
-// class FirebaseManager { // 기본 크루드 구현
-//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-//
-//   // Firestore 컬렉션에 데이터 저장
-//   Future<void> writeData(String collection, String docId, Map<String, dynamic> data) async {
-//     try {
-//       await _firestore.collection(collection).doc(docId).set(data);
-//     } catch (e) {
-//       throw Exception("Firestore write error: $e");
-//     }
-//   }
-//
-//   // Firestore에서 문서 읽기
-//   Future<DocumentSnapshot> readData(String collection, String docId) async {
-//     try {
-//       return await _firestore.collection(collection).doc(docId).get();
-//     } catch (e) {
-//       throw Exception("Firestore read error: $e");
-//     }
-//   }
-//
-//   // 실시간 데이터 감시
-//   Stream<DocumentSnapshot> listenToData(String collection, String docId) {
-//     try {
-//       return _firestore.collection(collection).doc(docId).snapshots();
-//     } catch (e) {
-//       throw Exception("Firestore listen error: $e");
-//     }
-//   }
-//
-//   // 하위 컬렉션에 데이터 저장
-//   Future<void> writeSubCollectionData(String collection, String docId, String subCollection, Map<String, dynamic> data) async {
-//     try {
-//       await _firestore.collection(collection).doc(docId).collection(subCollection).add(data);
-//     } catch (e) {
-//       throw Exception("Firestore write subcollection error: $e");
-//     }
-//   }
-//
-//   // 하위 컬렉션 실시간 데이터 감시
-//   Stream<QuerySnapshot> listenToSubCollection(String collection, String docId, String subCollection) {
-//     try {
-//       return _firestore.collection(collection).doc(docId).collection(subCollection).snapshots();
-//     } catch (e) {
-//       throw Exception("Firestore listen subcollection error: $e");
-//     }
-//   }
-// }
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseManager { // 기본 크루드 구현
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Firestore 컬렉션에 데이터 저장
+  Future<void> writeData(
+      String collection,
+      String docId, Map<String, dynamic> data
+      ) async {
+    try {
+      await _firestore.collection(collection).doc(docId).set(data);
+    } catch (e) {
+      throw Exception("Firestore write error: $e");
+    }
+  }
+
+  // Firestore에서 문서 읽기
+  Future<DocumentSnapshot> readData(String collection, String docId) async {
+    try {
+      return await _firestore.collection(collection).doc(docId).get();
+    } catch (e) {
+      throw Exception("Firestore read error: $e");
+    }
+  }
+
+  // 실시간 데이터 감시
+  Stream<DocumentSnapshot> listenToData(String collection, String docId) {
+    try {
+      return _firestore.collection(collection).doc(docId).snapshots();
+    } catch (e) {
+      throw Exception("Firestore listen error: $e");
+    }
+  }
+
+  // 하위 컬렉션에 데이터 저장
+  Future<void> writeSubCollectionData(
+      String collection, String docId,
+      String subCollection, Map<String, dynamic> data
+      ) async {
+    try {
+      await _firestore.collection(collection).doc(docId).collection(subCollection).add(data);
+    } catch (e) {
+      throw Exception("Firestore write subcollection error: $e");
+    }
+  }
+
+  // 하위 컬렉션 실시간 데이터 감시
+  Stream<QuerySnapshot> listenToSubCollection(
+      String collection,
+      String docId, String
+      subCollection) {
+    try {
+      return
+        _firestore.collection(collection).doc(docId).collection(subCollection).snapshots();
+    } catch (e) {
+      throw Exception("Firestore listen subcollection error: $e");
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:little_guardian/ui/views/child/setting/child_setting2_view.dart';
-import 'package:little_guardian/ui/views/components/practice_button.dart';
-import 'package:little_guardian/ui/views/parent/home/home_view.dart';
-import 'package:little_guardian/ui/views/splash_view.dart';
-import 'package:little_guardian/ui/views/parent/setting/parent_setting3_view.dart';
+// import 'package:little_guardian/ui/views/child/setting/child_setting2_view.dart';
+// import 'package:little_guardian/ui/views/components/practice_button.dart';
+// import 'package:little_guardian/ui/views/parent/home/home_view.dart';
+// import 'package:little_guardian/ui/views/splash_view.dart';
+import 'package:firebase_core/firebase_core.dart'; // Firebase Core 패키지 임포트
+import 'firebase_options.dart'; // Firebase 옵션 임포트
 import 'package:little_guardian/ui/views/parent/setting/setting1_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // 위젯 시스템 초기화
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // Firebase 초기화
+  );
   runApp(const MyApp());
 }
 
@@ -18,15 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        // primaryColor: Colors.blue,
-        // accentColor: Colors.amber,
-        // scaffoldBackgroundColor: Colors.white,
         fontFamily: 'NanumSquareNeo',
-        textTheme: TextTheme(
-          // headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          // headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-          // bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-        ),
       ),
       home: const Setting1View(),
       debugShowCheckedModeBanner: false,  // 디버그 배너 숨기기
